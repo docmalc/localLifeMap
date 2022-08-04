be4dd3fabd90f037c125fc6705eea8049fd31981
docmalc <51731051+docmalc@users.noreply.github.com>
2022-08-03 20:42:45 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -12,7 +12,7 @@ $(function() {
     fileType: 'csv',
     recordName: 'flu shot location',
     recordNamePlural: 'flu shot locations',
-    map_centroid: [41.85754, -87.66231],
+    map_centroid: [33.928287, -84.329268],
     defaultZoom:  11,
     defaultRadius: 1610,
     debug: false,
